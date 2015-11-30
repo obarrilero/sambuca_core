f971f47d37e297a87f0731571810c2414f011203
Daniel Collins <daniel.collins@csiro.au>
2015-11-30 16:23:05 +0800

    Bump version: 0.1.2 → 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 project = u'Sambuca'
 copyright = u'2014-15, CSIRO'
-version = release = '0.1.2'
+version = release = '0.1.3'
 pygments_style = 'perldoc'
 templates_path = ['.']
 html_use_smartypants = True
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PyTest(TestCommand):
 
 setup(
     name='sambuca-core',
-    version='0.1.2',
+    version='0.1.3',
     description='Core components for the SAMBUCA model',
     long_description=long_description,
 
