819af414278a0729f03efd7a66a351bffb0a9021
Daniel Collins <daniel.collins@csiro.au>
2015-12-03 16:49:36 +0800

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PyTest(TestCommand):
 
 setup(
     name='sambuca-core',
-    version='1.0.0',
+    version='1.1.0',
     description='Core components for the SAMBUCA model',
     long_description=long_description,
 
